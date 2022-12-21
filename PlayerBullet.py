becaee0ebcedf90f63a14444c242f22c0d6097eb
Vitor Daniel <vitor.daniel@outlook.pt>
2022-12-21 18:36:10 +0000

    started changing player class

--- PlayerBullet.py
+++ PlayerBullet.py
@@ -1,9 +1,10 @@
 # program file for Player Movement and Bullets
 #criated by Vitor Daniel, nº a22204782 and João Carvalho, nº a22204909
 
-import pygame
-import math
-import numpy as np
+#fetch libraries from Imports, where all libraries needed are stored
+from Imports import *
+
+window_width = SCREEN.get_width
 
 #colors
 white = (255, 255, 255)
@@ -22,7 +23,7 @@ blue = (math.sin(ang) + 1) / 2 * 255
 green = ((math.cos(ang)) * (math.sin(ang)) + 1) / 2 * 255
 Color = (red, green, blue)
 
-#keep rainbow looping
+#keep "rainbow" looping
 if (red > 255):
     red = 0
 
@@ -32,61 +33,15 @@ if (green > 255):
 if (blue > 255):
     blue = 0
 
-#screen size
-Window_width = 800
-Window_height = 600
-
-#draw screen
-Window = pygame.display.set_mode((Window_width, Window_height))
-pygame.display.set_caption("Asteroids")
-
-playerSpeed = 15
+#player variables
+playerSpeed = 10
 playerMaxSpeed = 20
 playerMaxRotation = 10
 playerColor = white
 playerSize = 10
 
 class Player:
-    #adapt code  to my own specifications and requirements, add images
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.hspeed = 0
-        self.vspeed = 0
-        self.dir = -90
-        self.rtspd = 0
-        self.thrust = False
-
-    def updatePlayer(self):
-        # Move player
-        speed = math.sqrt(self.hspeed**2 + self.vspeed**2)
-        if self.thrust:
-            if speed + fd_fric < player_max_speed:
-                self.hspeed += fd_fric * math.cos(self.dir * math.pi / 180)
-                self.vspeed += fd_fric * math.sin(self.dir * math.pi / 180)
-            else:
-                self.hspeed = player_max_speed * math.cos(self.dir * math.pi / 180)
-                self.vspeed = player_max_speed * math.sin(self.dir * math.pi / 180)
-        else:
-            if speed - bd_fric > 0:
-                change_in_hspeed = (bd_fric * math.cos(self.vspeed / self.hspeed))
-                change_in_vspeed = (bd_fric * math.sin(self.vspeed / self.hspeed))
-                if self.hspeed != 0:
-                    if change_in_hspeed / abs(change_in_hspeed) == self.hspeed / abs(self.hspeed):
-                        self.hspeed -= change_in_hspeed
-                    else:
-                        self.hspeed += change_in_hspeed
-                if self.vspeed != 0:
-                    if change_in_vspeed / abs(change_in_vspeed) == self.vspeed / abs(self.vspeed):
-                        self.vspeed -= change_in_vspeed
-                    else:
-                        self.vspeed += change_in_vspeed
-            else:
-                self.hspeed = 0
-                self.vspeed = 0
-        self.x += self.hspeed
-        self.y += self.vspeed
-
+    
         # Check for wrapping
         if self.x > display_width:
             self.x = 0
@@ -101,47 +56,13 @@ class Player:
         self.dir += self.rtspd
 
     def drawPlayer(self):
-        a = math.radians(self.dir)
-        x = self.x
-        y = self.y
-        s = player_size
+        ang = math.radians(self.dir)
         t = self.thrust
         # Draw player
-        pygame.draw.line(Window, white,
-                         (x - (s * math.sqrt(130) / 12) * math.cos(math.atan(7 / 9) + a),
-                          y - (s * math.sqrt(130) / 12) * math.sin(math.atan(7 / 9) + a)),
-                         (x + s * math.cos(a), y + s * math.sin(a)))
-
-        pygame.draw.line(Window, white,
-                         (x - (s * math.sqrt(130) / 12) * math.cos(math.atan(7 / 9) - a),
-                          y + (s * math.sqrt(130) / 12) * math.sin(math.atan(7 / 9) - a)),
-                         (x + s * math.cos(a), y + s * math.sin(a)))
-
-        pygame.draw.line(Window, white,
-                         (x - (s * math.sqrt(2) / 2) * math.cos(a + math.pi / 4),
-                          y - (s * math.sqrt(2) / 2) * math.sin(a + math.pi / 4)),
-                         (x - (s * math.sqrt(2) / 2) * math.cos(-a + math.pi / 4),
-                          y + (s * math.sqrt(2) / 2) * math.sin(-a + math.pi / 4)))
-        if t:
-            pygame.draw.line(Window, white,
-                             (x - s * math.cos(a),
-                              y - s * math.sin(a)),
-                             (x - (s * math.sqrt(5) / 4) * math.cos(a + math.pi / 6),
-                              y - (s * math.sqrt(5) / 4) * math.sin(a + math.pi / 6)))
-            pygame.draw.line(Window, white,
-                             (x - s * math.cos(-a),
-                              y + s * math.sin(-a)),
-                             (x - (s * math.sqrt(5) / 4) * math.cos(-a + math.pi / 6),
-                              y + (s * math.sqrt(5) / 4) * math.sin(-a + math.pi / 6)))
-
-    def killPlayer(self):
-        # Reset the player
-        self.x = display_width / 2
-        self.y = display_height / 2
-        self.thrust = False
-        self.dir = -90
-        self.hspeed = 0
-        self.vspeed = 0
+        PLAYER = pygame.image.load("Images/Spaceship.png")
+        PLAYER_x = self.x
+        PLAYER_y = self.y
+        
 
 #player needs image and movement (adapt any good ideas from this code above)
 
