44831e62e42834be827e5c9b77b68fc5141143a3
JoaoCarvalho2002 <jonitkick@gmail.com>
2022-12-23 21:15:58 +0000

    Menu-comentários

--- Menu.py
+++ Menu.py
@@ -1,3 +1,4 @@
+#mports necessarios
 import pygame, sys
 from pygame.locals import *
 import random
@@ -5,7 +6,7 @@ from Imports import *
 from Meteor import *
 pygame.init()
 
-
+#funçao que verifica se meteoro do menu saiu do ecrã
 def outofscreen(c):            
     if c>=screen.x or c<=(0,0):
         n1=random.uniform(0.1,550)
@@ -14,15 +15,14 @@ def outofscreen(c):
         if n1<=230:
             meteor.mudX=n1
             meteor.mudY=5
-            print(meteor.mudX,meteor.mudY)
         else:        
             meteor.mudX=5
             meteor.mudY=n1
-            print(meteor.mudX,meteor.mudY)
+            
 def Menufunc(): 
         while True:  
                 for event in pygame.event.get():
-                        #posição do mouse    
+                        #posição do constante mouse    
                         if event.type==pygame.MOUSEMOTION:
                                 #posição do mouse    
                                 pos=pygame.mouse.get_pos()    
@@ -30,23 +30,28 @@ def Menufunc():
 
                                 #start button
                                 if start_but.collidepoint(pos):
-                                        
+                                        #butão start e o "blit" "acende" o butão quando o rato passa por cima
                                         pygame.draw.rect(SCREEN,WHITE,(305,200,190,70))
                                         SCREEN.blit(textstart,(350,215))
                                         pygame.display.update()
+                                        #se o butão for carregado
                                         if pygame.mouse.get_pressed()[0] and start_but.collidepoint(pos):
                                                 print("Move on")
+                                                #chama função que começa o jogo
                                                 spawnmeteor()
                                                 meteor.auvar=True
                                                 
                                         
                                 #exit button
                                 if exit_but.collidepoint(pos):
+                                        #butão start e o "blit" "acende" o butão quando o rato passa por cima
                                         SCREEN.blit(textquit,(350,335))
                                         pygame.draw.rect(SCREEN,WHITE,(305,320,190,70))
                                         pygame.display.update()
+                                        #se o butão for carregado
                                         if pygame.mouse.get_pressed()[0] and exit_but.collidepoint(pos):
                                                 SCREEN.blit(textquit,(350,335))
+                                                #sai do jogo
                                                 print("quit")
                                                 pygame.quit()   
                                                 sys.exit()
@@ -54,6 +59,7 @@ def Menufunc():
                 SCREEN.fill((0,0,0)) 
                 meteor.x=meteor.x+0.1
                 meteor.y=meteor.y+0.1
+                #pontos brancos imoveis no fundo do ecrã para simular ESTRELAS
                 pygame.draw.circle(SCREEN,WHITE,[180,230],3)
                 pygame.draw.circle(SCREEN,WHITE,[400,120],2)
                 pygame.draw.circle(SCREEN,WHITE,[550,500],6)
@@ -68,9 +74,10 @@ def Menufunc():
                 pygame.draw.circle(SCREEN,WHITE,[760,400],2)
                 pygame.draw.circle(SCREEN,WHITE,[160,532],7)
 
-                                #METEORITOS NO SCREEN
+                #METEORITOS NO SCREEN
                 pygame.draw.circle(SCREEN,color_dark,[meteor.x+meteor.mudX,meteor.y+meteor.mudY],20)
                 c=(meteor.x,meteor.y)
+                #chama função para verificar se está fora do ecra
                 outofscreen(c)
                 pygame.draw.circle(SCREEN,color_dark,[meteor.x-meteor.mudX,meteor.y-meteor.mudY],25)
                 c=(meteor.x,meteor.y)
@@ -79,17 +86,20 @@ def Menufunc():
                 c=(meteor.x,meteor.y)
                 outofscreen(c)
 
-                                #meteores
+                #meteores
                 pygame.draw.rect(SCREEN,color_shade,(270,50,260,100))
                 pygame.draw.circle(SCREEN,color_dark,[310,85],10)
                 pygame.draw.circle(SCREEN,color_dark,[485,115],30)
+                #highlight text
                 SCREEN.blit(textComets,(310,80))
                                 
-                                #startbutton
+                #startbutton default sem interferencia do MOUSE
                 pygame.draw.rect(SCREEN,color_shade,(305,200,190,70))
+                #highlight text
                 SCREEN.blit(textstart,(350,215))
-                                #exitbut
+                #exitbut default sem interferencia do MOUSE
                 pygame.draw.rect(SCREEN,color_shade,(305,320,190,70))
+                #highlight text
                 SCREEN.blit(textquit,(350,335))      
 
                 pygame.display.update()
