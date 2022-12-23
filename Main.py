e476878164d0a845c2f20d51bba53e1691cf1fbb
JoaoCarvalho2002 <jonitkick@gmail.com>
2022-12-23 21:16:51 +0000

    Main-comentários

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from Imports import *
 from Menu import *
 from Player import *
 pygame.init()
+#if para quando se carregar no butão start irá passar para o proximo ecra(não funcional)
 if meteor.auvar==False:           
     Menufunc()
 else:    
